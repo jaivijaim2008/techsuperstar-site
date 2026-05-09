46cde786629519cd3c44c85ba632ded2e5535139
Jai-vijai <jaitnea@gmail.com>
2026-05-09 15:12:06 +0530

    Fix sanity config import all schema types

---
+++ dist/static/sanity-IYdR0i5y.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./pane2-BKqIYc7H.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { postType } from "./sanity/schemaTypes/postType";
+import { authorType } from "./sanity/schemaTypes/authorType";
+import { blockContentType } from "./sanity/schemaTypes/blockContentType";
+import { categoryType } from "./sanity/schemaTypes/categoryType";
+import { commentType } from "./sanity/schemaTypes/commentType";
 
 export default defineConfig({
   name: "default",
@@ -15,31 +19,6 @@ export default defineConfig({
   plugins: [structureTool(), visionTool()],
 
   schema: {
-    types: [
-      {
-        name: "comment",
-        title: "Comment",
-        type: "document",
-        fields: [
-          { name: "name", title: "Name", type: "string" },
-          { name: "email", title: "Email", type: "string" },
-          { name: "message", title: "Message", type: "text" },
-          { name: "approved", title: "Approved", type: "boolean" },
-          { name: "post", title: "Post", type: "reference", to: [{ type: "post" }] },
-          { name: "createdAt", title: "Created At", type: "datetime" },
-        ],
-      },
-      {
-        name: "category",
-        title: "Category",
-        type: "document",
-        fields: [
-          { name: "title", title: "Title", type: "string" },
-          { name: "slug", title: "Slug", type: "slug", options: { source: "title" } },
-          { name: "description", title: "Description", type: "text" },
-        ],
-      },
-      postType, // ✅ uses postType.ts which has specs, pros, cons
-    ],
+    types: [postType, authorType, blockContentType, categoryType, commentType],
   },
 });
