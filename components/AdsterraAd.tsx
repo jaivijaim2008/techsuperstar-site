4ca0cba23b0ef800073f75184e8b3496e333b379
Jai-vijai <jaitnea@gmail.com>
2026-05-10 12:26:41 +0530

    fix: Adsterra ad script injection

--- components/AdsterraAd.tsx
+++ components/AdsterraAd.tsx
@@ -9,18 +9,46 @@ interface Props {
 
 export default function AdsterraAd({ adKey, width, height }: Props) {
   const ref = useRef<HTMLDivElement>(null);
+  const injected = useRef(false);
 
   useEffect(() => {
-    if (!ref.current || ref.current.childNodes.length > 0) return;
+    if (!ref.current || injected.current) return;
+    injected.current = true;
+
+    const container = ref.current;
+
     const s1 = document.createElement("script");
-    s1.text = `atOptions={'key':'${adKey}','format':'iframe','height':${height},'width':${width},'params':{}};`;
+    s1.type = "text/javascript";
+    s1.innerHTML = `
+      var atOptions = {
+        'key': '${adKey}',
+        'format': 'iframe',
+        'height': ${height},
+        'width': ${width},
+        'params': {}
+      };
+    `;
+    container.appendChild(s1);
+
     const s2 = document.createElement("script");
+    s2.type = "text/javascript";
     s2.src = `https://www.highperformanceformat.com/${adKey}/invoke.js`;
-    ref.current.appendChild(s1);
-    ref.current.appendChild(s2);
-  }, []);
+    s2.async = true;
+    container.appendChild(s2);
+  }, [adKey, width, height]);
 
   return (
-    <div ref={ref} style={{ width: "100%", display: "flex", justifyContent: "center", margin: "20px 0" }} />
+    <div
+      ref={ref}
+      style={{
+        width: "100%",
+        minHeight: `${height}px`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "20px 0",
+        overflow: "hidden",
+      }}
+    />
   );
 }
